663ef1111fdaccb700d060f618b48115d0cafd6e
Joshua Smith <kognate@gmail.com>
2009-09-07 23:26:54 -0400

    implemented pattern for normal form

--- lib/phonenormalize.rb
+++ lib/phonenormalize.rb
@@ -1,8 +1,9 @@
 class Phonenormalize
-  attr_accessor :phone_number
+  attr_accessor :phone_number, :normal_form_pattern
   VERSION = '1.0.0'
   def initialize(pnum = nil)
     self.phone_number = pnum
+    self.normal_form_pattern = "(%A) %E-%C"
   end
 
   def is_valid?
@@ -16,7 +17,10 @@ class Phonenormalize
   def normal_form
     _m = self.phone_number.gsub(/[^\d]/,"").match(/(1?[2-9][0-8][0-9])([0-9]{3})([0-9]{4})/)
     if _m
-      sprintf("(%s) %s-%s",_m[1],_m[2],_m[3])
+      _res = self.normal_form_pattern.gsub(/%A/,_m[1])
+      _res.gsub!(/%E/,_m[2])
+      _res.gsub!(/%C/,_m[3])
+      _res
     else
       raise BadPhoneNumber.new("Coulnd't find valid phone number in #{self.phone_number}")
     end
